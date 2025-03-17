21285034340a0786db47b30dbf67a45ed8bc4eb9
nouman <dev009.binarybrix@gmail.com>
2025-03-18 04:06:01 +0500

    updated code

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -20,6 +20,8 @@ import Alert from '@mui/material/Alert'
 
 // Third-party Imports
 // import { signIn } from 'next-auth/react'
+// import { signIn } from 'next-auth/react'
+
 import { Controller, useForm } from 'react-hook-form'
 import { valibotResolver } from '@hookform/resolvers/valibot'
 import { object, minLength, string, email, pipe, nonEmpty } from 'valibot'
