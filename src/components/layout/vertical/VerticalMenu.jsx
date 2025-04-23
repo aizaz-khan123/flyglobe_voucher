4690d6c1ed73c2ecd7757cf7a94b2813f19e3abf
aizaz-khan123 <122221556+aizaz-khan123@users.noreply.github.com>
2025-04-23 10:16:00 +0500

    hide import pnr

--- src/components/layout/vertical/VerticalMenu.jsx
+++ src/components/layout/vertical/VerticalMenu.jsx
@@ -81,14 +81,14 @@ const VerticalMenu = ({ dictionary, scrollMenu }) => {
           >
             {dictionary['navigation'].flight}
           </MenuItem>
-          <MenuItem
+          {/* <MenuItem
             href={`/${locale}/apps/import-pnr`}
             exactMatch={false}
             activeUrl='/apps/import-pnr'
             icon={<i className='ri-import-line' />}
           >
             {dictionary['navigation'].importPnr}
-          </MenuItem>
+          </MenuItem> */}
           <MenuItem
             href={`/${locale}/apps/bookings`}
             exactMatch={false}
