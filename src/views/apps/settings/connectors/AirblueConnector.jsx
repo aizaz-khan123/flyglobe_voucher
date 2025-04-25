4fc53b6b602db90ce158833513c678b7e4b24bc7
aizaz-khan123 <122221556+aizaz-khan123@users.noreply.github.com>
2025-04-25 22:31:00 +0500

    airblue connector

---
+++ src/views/apps/settings/connectors/AirblueConnector.jsx
@@ -0,0 +1,179 @@
+'use client'
+
+import { useEffect } from 'react'
+
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Controller, useForm } from 'react-hook-form'
+
+import { Button, Card, CardContent, FormControlLabel, FormLabel, Switch } from '@mui/material'
+
+import { toast } from 'react-toastify'
+
+import { useGetSupplierListQuery, useShowConnectorQuery, useUpdateConnectorMutation } from '@/redux-store/services/api'
+
+import MuiTextField from '@/components/mui-form-inputs/MuiTextField'
+import MuiDropdown from '@/components/mui-form-inputs/MuiDropdown'
+import { LoadingButton } from '@mui/lab';
+
+const AirblueConnector = () => {
+    const { data: supplierDropDown } = useGetSupplierListQuery()
+    const { data: connector, isSuccess: isConnectorSuccess } = useShowConnectorQuery('AIRBLUE_API')
+    const [updateConnector, { isLoading: isUpdateLoading }] = useUpdateConnectorMutation()
+
+    const connectorSchema = z.object({
+        api_key: z.string({ required_error: 'API_ID Required!' }),
+        api_secret: z.string({ required_error: 'API_PASSWORD Required!' }),
+        connector_domain: z.string({ required_error: 'API Client ID/KEY Required!' }),
+        supplier_id: z.number({ required_error: 'Supplier ID Required!' }).min(1, 'Supplier ID is required!'),
+        is_enable: z.boolean()
+    })
+
+    const { control, handleSubmit, reset } = useForm({
+        resolver: zodResolver(connectorSchema)
+    })
+
+    useEffect(() => {
+        if (isConnectorSuccess && connector) {
+            reset({
+                api_key: connector.api_key,
+                api_secret: connector.api_secret,
+                connector_domain: connector.connector_domain,
+                is_enable: connector.is_enable,
+                supplier_id: connector.supplier_id
+            })
+        }
+    }, [connector, isConnectorSuccess, reset])
+
+    const onSubmit = handleSubmit(async data => {
+        const updated_data = {
+            ...data,
+            name: 'AIRBLUE',
+            type: 'AIRBLUE_API'
+        }
+
+        await updateConnector(updated_data).then(response => {
+            if (response.data?.code == 200) {
+                toast.success('AIRBLUE API Credentials Updated!');
+            }
+        })
+    })
+
+    return (
+        <>
+            <Card className='mt-5 bg-base-100'>
+                <CardContent className={'p-0'}>
+                    <CardContent className='gap-0'>
+                        <div>AIRBLUE API Credentials</div>
+                        <div className='mt-1 grid grid-cols-1 gap-5 gap-y-3 md:grid-cols-2'>
+                            <div>
+                                <FormLabel title={'API ID'} htmlFor='api_key'></FormLabel>
+                                <MuiTextField
+                                    className='w-full border-0 focus:outline-0'
+                                    control={control}
+                                    size='md'
+                                    id='api_key'
+                                    name='api_key'
+                                    placeholder='Enter API ID'
+                                />
+                            </div>
+                            <div>
+                                <FormLabel title={'API PASSWORD'} htmlFor='api_secret'></FormLabel>
+                                <MuiTextField
+                                    className='w-full border-0 focus:outline-0'
+                                    control={control}
+                                    size='md'
+                                    id='api_secret'
+                                    name='api_secret'
+                                    placeholder='Enter API PASSWORD'
+                                />
+                            </div>
+                            <div>
+                                <FormLabel title={'APIClient ID/key'} htmlFor='connector_domain'></FormLabel>
+                                <MuiTextField
+                                    className='w-full border-0 focus:outline-0'
+                                    control={control}
+                                    size='md'
+                                    id='connector_domain'
+                                    name='connector_domain'
+                                    placeholder='Enter APIClient ID/key'
+                                />
+                            </div>
+                            <div>
+                                <FormLabel title='Supplier' htmlFor='supplier_id' />
+                                {supplierDropDown ? (
+                                    <MuiDropdown
+                                        control={control}
+                                        name='supplier_id'
+                                        size='md'
+                                        id='supplier_id'
+                                        className='w-full border-0 text-base'
+                                        options={supplierDropDown.map(supplier => ({
+                                            label: supplier.name,
+                                            value: supplier.id
+                                        }))}
+                                        placeholder='Select Supplier'
+                                    />
+                                ) : (
+                                    <p>Loading...</p>
+                                )}
+                            </div>
+
+                            <div>
+                                <div>
+                                    <Controller
+                                        name="is_enable"
+                                        control={control}
+                                        render={({ field }) => (
+                                            <FormControlLabel
+                                                control={
+                                                    <Switch
+                                                        {...field}
+                                                        checked={!!field.value}
+                                                        color="primary"
+                                                    />
+                                                }
+                                                label="Status"
+                                            />
+                                        )}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className=' flex justify-between gap-6'>
+                            <div></div>
+                            <div className='mt-6 flex justify-end gap-6'>
+                                {/* <Button
+                                    color='primary'
+                                    size='md'
+                                    onClick={onSubmit}
+
+                                    // startIcon={<Icon icon={checkIcon} fontSize={18} />}
+                                    loading={isUpdateLoading}
+                                    variant='contained'
+                                    className='mt-2'
+                                >
+                                    Update
+                                </Button> */}
+
+                                <LoadingButton
+                                    color='primary'
+                                    size='md'
+                                    onClick={onSubmit}
+                                    loading={isUpdateLoading}
+                                    variant='contained'
+                                    className='mt-2'
+                                >
+                                    Update
+                                </LoadingButton>
+                            </div>
+                        </div>
+                    </CardContent>
+                </CardContent>
+            </Card>
+        </>
+    )
+}
+
+export { AirblueConnector }
